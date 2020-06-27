a535d31d061d5af49efe316b54c1cc1f827ae596
Akshay <akshaysawant8450@gmail.com>
2020-06-27 14:00:12 +0000

    added sanitizer to code

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Campground = require('../models/campground');
 const middleware = require("../middleware");
+const expressSanitizer = require('express-sanitizer');
 
 //all campgrounds
 router.get('/', (req, res) => {
@@ -98,7 +99,7 @@ router.post('/', middleware.isLoggedIn, (req, res) => {
 		let name = req.body.name;
 		let image = req.body.image;
 		let price = req.body.price;
-		let desc = req.body.description;
+		let desc = req.sanitize(req.body.description);
 		let address = req.body.address;
 		let nlocation = req.body.nlocation;
 		let email = req.body.email;
@@ -158,7 +159,7 @@ router.put('/:id', middleware.checkCampgroundOwnership, (req, res) => {
 			data.name = req.body.name;
 			data.price = req.body.price;
 			data.image = req.body.image;
-			data.desc = req.body.description;
+			data.desc = req.sanitize(req.body.description);
 			data
 				.save()
 				.then(data => {
